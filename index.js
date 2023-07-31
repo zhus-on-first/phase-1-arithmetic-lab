1ef6e3ddc9a7bcc1a16b34ba999a5d74722fefb5
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-07-31 15:36:46 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// multiply
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+//random integer
+let random = Math.floor(Math.random() * 10);
+
+//remainder
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+//max
+let max = Math.max(1, 3, 5, 20);
